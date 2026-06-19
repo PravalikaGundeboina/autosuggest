92b91d9fa717519a8cdb0e29463988a865b0b07a
Pravalika <pravalikayadav0212@gmail.com>
2026-06-19 13:47:45 +0530

    fixed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 6700;
 
 //To tell the app where static files - html/css/js are stored
-app.use(express.static("Frontend"));
+app.use(express.static("frontend"));
 
 app.listen(port , function(){
     console.log("App running on http://localhost:" + port);
